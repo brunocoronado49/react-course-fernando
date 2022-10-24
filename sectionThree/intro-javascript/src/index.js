24cfd588b1158f415d4c1bcd715d17511c5097c5
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-10-24 15:47:44 -0500

    Terminando la intro de js

--- sectionThree/intro-javascript/src/index.js
+++ sectionThree/intro-javascript/src/index.js
@@ -1,1 +1,16 @@
-/// Operador ternario
+/// Operador condicional ternario
+const activo = true;
+
+// let mensaje = "";
+
+// if (activo) {
+//     mensaje = "activo";
+// } else {
+//     mensaje = "inactivo";
+// }
+
+const mensaje = activo ? "Activo" : "Inactivo";
+const mensaje2 = activo && "Activo";
+
+console.log(mensaje);
+console.log(mensaje2);
