5fbf0579b3ff62e1547af8ab41458eec79a3a849
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-10-25 22:06:38 -0500

    Impresion variables

--- sectionFour/counter-app/src/FirstApp.jsx
+++ sectionFour/counter-app/src/FirstApp.jsx
@@ -1,7 +1,19 @@
+const newMessage = {
+    message: "Hola mundo",
+    title: "Burton",
+};
+
+/// Se recomienda usar funciones fuera del funcional
+/// component para que no guarde espacio en memoria
+const message = () => {
+    return "Hola mundo";
+};
+
 export const FirstApp = () => {
     return (
         <>
-            <h1>Francisco</h1>
+            <h1>Bruno</h1>
+            {/* <code>{JSON.stringify(newMessage)}</code> */}
             <p>Soy un subtitulo</p>
         </>
     );
