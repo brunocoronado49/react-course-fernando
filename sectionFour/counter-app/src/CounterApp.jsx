f194ceed0c147b67fa7f8a7ddbb65be2d6566952
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-10-30 13:06:21 -0600

    Preparando para las pruebas

--- sectionFour/counter-app/src/CounterApp.jsx
+++ sectionFour/counter-app/src/CounterApp.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
-    const handleAdd = () => {
-        setCounter(counter + 1);
-    };
+    const handleAdd = () => setCounter(counter + 1);
+    const handleRest = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{counter}</h2>
             <button onClick={handleAdd}>+1</button>
+            <button onClick={handleRest}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 };
@@ -24,3 +26,4 @@ CounterApp.propTypes = {
 CounterApp.defaultProps = {
     value: 0,
 };
+ 
--- sectionThree/intro-javascript/src/index.js
+++ sectionThree/intro-javascript/src/index.js
@@ -1,14 +1,6 @@
 /// Operador condicional ternario
 const activo = true;
 
-// let mensaje = "";
-
-// if (activo) {
-//     mensaje = "activo";
-// } else {
-//     mensaje = "inactivo";
-// }
-
 const mensaje = activo ? "Activo" : "Inactivo";
 const mensaje2 = activo && "Activo";
 
