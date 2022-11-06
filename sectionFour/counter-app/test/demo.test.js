09f7f8bac57629dcbfe93e7dfdcaebbd2ec885d0
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-11-06 01:13:49 -0600

    Pruebas hechas correctamente, preparando para el metodo toEqual

--- sectionFour/counter-app/test/demo.test.js
+++ sectionFour/counter-app/test/demo.test.js
@@ -1,5 +1,14 @@
-test('Esta prueba no debe de fallar', () => {
-    if (1 === 0) {
-        throw new Error('No puede dividir entre 0')
-    }
-})
+/// Pruebas en ese componente, el describe es para darle
+/// una descripcion o titulos a las pruebas
+describe("Pruebas en <DemoComponent />", () => {
+    test("Esta prueba no debe de fallar", () => {
+        /// 1. Inicialización
+        const msgOne = "Hello World";
+
+        /// 2. Estimulo
+        const msgTwo = msgOne.trim();
+
+        /// 3. Observar el comportamiento
+        expect(msgOne).toBe(msgTwo);
+    });
+});
